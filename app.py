6c6962e1a21ff88c3285343a2a6f28de6fb81e1f
toneesha <73363440+toneesha@users.noreply.github.com>
2020-10-24 20:04:20 +0400

    Update app.py

--- app.py
+++ app.py
@@ -206,4 +206,4 @@ def update_result(age, cp, tbps, chol, recg, thalach, texang, oldpeak):
 
 #import os from pml import app port = int(os.environ.get('PORT', 5000))
 #app.run()
-app.run(port = int(os.environ.get('PORT', 5000)))
+app.run(debug = True, port = int(os.environ.get('PORT', 5000)))
