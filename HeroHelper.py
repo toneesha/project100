90196687d79e0a651711f86f74d3e2cc7bb58548
toneesha <73363440+toneesha@users.noreply.github.com>
2020-10-24 15:58:56 +0400

    Update HeroHelper.py

--- HeroHelper.py
+++ HeroHelper.py
@@ -4,11 +4,11 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 #import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 #from sklearn.metrics import accuracy_score
 #import matplotlib.pyplot as plt
-import plotly.express as px
+#import plotly.express as px
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
